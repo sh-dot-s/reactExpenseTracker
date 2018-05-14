de22ea3eee0d2a722f5d96783d1fd630dda56694
Sankarasetty Lokesh Sai Sri Harsha <SankarsettyLokesh.SaiSriHarsha@mrcooper.com>
2018-05-14 10:48:27 +0530

    Sample Commit

--- scripts/app.js
+++ scripts/app.js
@@ -23,6 +23,11 @@ const expense2 = store.dispatch(actions.addExpense({
     comments: "Power consumption charges for month January and February",
     amount: 2000,
 }));
+const expense3 = store.dispatch(actions.addExpense({
+    description: "Monthly Rent",
+    comments: "Amount to be paid to the House Owner before 5th of every month",
+    amount: 14000,
+}));
 
 const element = (
     <Provider store={store}>
